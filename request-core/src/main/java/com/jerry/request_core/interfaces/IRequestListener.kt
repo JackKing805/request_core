52d1a65e30d5972eb8a3a518ad23b80fd55a8f89
Mallow <45876815+JackKing805@users.noreply.github.com>
2023-05-18 19:01:24 -0700

    Update IRequestListener.kt

--- request-core/src/main/java/com/jerry/request_core/interfaces/IRequestListener.kt
+++ request-core/src/main/java/com/jerry/request_core/interfaces/IRequestListener.kt
@@ -1,11 +1,13 @@
 package com.jerry.request_core.interfaces
 
 import com.jerry.request_core.constants.Status
+import com.jerry.rt.core.http.pojo.Request
+import com.jerry.rt.core.http.pojo.Response
 
 interface IRequestListener {
     fun onStatusChange(status: Status)
 
-    fun onRequest(url:String){}
+    fun onRequest(request:Request,response:Response,url:String){}
 
     sealed class AuthResult{
         object Grant : AuthResult()
